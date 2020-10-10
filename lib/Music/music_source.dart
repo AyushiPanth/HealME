0fd9aa39d82a97747ec8229e176ef0d0826e18f6
AyushiPanth <lit2019073@iiitl.ac.in>
2020-10-10 15:02:55 +0530

    added music source

---
+++ lib/Music/music_source.dart
@@ -0,0 +1,14 @@
+class Musique {
+  String titre;
+  String auteur;
+  String imagePath;
+  String musicUrl;
+
+  Musique(String title, String auteur, String imagePath, String musicUrl) {
+    this.titre = title;
+    this.auteur = auteur;
+    this.imagePath = imagePath;
+    this.imagePath = imagePath;
+    this.musicUrl = musicUrl;
+  }
+}
